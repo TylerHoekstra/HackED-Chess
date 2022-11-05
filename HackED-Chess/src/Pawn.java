6dbcf8529c90fe7c66bd8de08bc2432a41c96077
TylerHoekstra <89994695+TylerHoekstra@users.noreply.github.com>
2022-11-05 14:56:29 -0600

    Pawn getPossibleMoves done?

--- HackED-Chess/src/Pawn.java
+++ HackED-Chess/src/Pawn.java
@@ -18,22 +18,42 @@ public class Pawn extends Piece{
 		int x = this.getPosition()[0];
 		int y = this.getPosition()[1];
 		possibleMoves.clear();
+		
+		// For Black
 		if(isBlack) {
+			// Checks moving Forward
 			if(theBoard.getPieceAtPosition(new int[]{x,y-1}) == 0) {
 				if(!hasMoved && theBoard.getPieceAtPosition(new int[]{x,y-2}) == 0) {
 					possibleMoves.add(String.valueOf(x) + String.valueOf(y-2));
 				}
 				possibleMoves.add(String.valueOf(x) + String.valueOf(y-1));
 			}
+			
+			// Checks Attack
+			if(theBoard.getPieceAtPosition(new int[]{x-1,y-1}) == -1) {
+				possibleMoves.add(String.valueOf(x-1) + String.valueOf(y-1));
+			}
+			if(theBoard.getPieceAtPosition(new int[]{x+1,y-1}) == -1) {
+				possibleMoves.add(String.valueOf(x+1) + String.valueOf(y-1));
+			}
 		}
+		// For white
 		else {
+			// Checks Moving Forward
 			if(theBoard.getPieceAtPosition(new int[]{x,y+1}) == 0) {
 				if(!hasMoved && theBoard.getPieceAtPosition(new int[]{x,y+2}) == 0) {
-					possibleMoves.add(String.valueOf(x) + String.valueOf(y+2));;
+					possibleMoves.add(String.valueOf(x) + String.valueOf(y+2));
 				}
-				possibleMoves.add(String.valueOf(x) + String.valueOf(y+1));
+				possibleMoves.add(String.valueOf(x) + String.valueOf(y+1));	
+			}
+			
+			// Checks Attack
+			if(theBoard.getPieceAtPosition(new int[]{x-1,y+1}) == 1) {
+				possibleMoves.add(String.valueOf(x-1) + String.valueOf(y+1));
+			}
+			if(theBoard.getPieceAtPosition(new int[]{x+1,y+1}) == 1) {
+				possibleMoves.add(String.valueOf(x+1) + String.valueOf(y+1));
 			}
 		}
 	}
-
 }
