942dc990e87382416a9e52f4d98c24bafa713779
TylerHoekstra <89994695+TylerHoekstra@users.noreply.github.com>
2022-11-05 16:00:34 -0600

    d

--- HackED-Chess/src/Game.java
+++ HackED-Chess/src/Game.java
@@ -1,13 +1,9 @@
-import java.util.ArrayList;
-
 public class Game {
-
-	public static void startGame() {
-		ArrayList<Piece> allPieces = new ArrayList<Piece>();
-	}
 	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Board newGame = new Board();
+		Player
+		Player
 
 	}
 
