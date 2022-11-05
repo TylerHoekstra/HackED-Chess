60a6ddc80b56352926a3ddae6476f25dd285d2b1
TylerHoekstra <89994695+TylerHoekstra@users.noreply.github.com>
2022-11-05 15:46:30 -0600

    Rook find possible moves

--- HackED-Chess/src/Rook.java
+++ HackED-Chess/src/Rook.java
@@ -12,8 +12,46 @@ public class Rook extends Piece{
 
 	@Override
 	public void findPossibleMoves(Board theBoard) {
-		
-		
+		int x = this.getPosition()[0];
+		int y = this.getPosition()[1];
+		possibleMoves.clear();
+		int z;
+		if(isBlack)
+			z = 1;
+		else
+			z = -1;
+		// Right
+		for(int i = x; i < 8; i++) {
+			if(theBoard.getPieceAtPosition(new int[]{i,y}) != z) {
+				possibleMoves.add(String.valueOf(i) + String.valueOf(y));
+			}
+			else
+				break;
+		}
+		// Left
+		for(int i = x; i > 0; i--) {
+			if(theBoard.getPieceAtPosition(new int[]{i,y}) != z) {
+				possibleMoves.add(String.valueOf(i) + String.valueOf(y));
+			}
+			else
+				break;
+		}
+		// Up
+		for(int i = y; i < 8; i++) {
+			if(theBoard.getPieceAtPosition(new int[]{x,i}) != z) {
+				possibleMoves.add(String.valueOf(x) + String.valueOf(i));
+			}
+			else
+				break;
+		}
+		// Down
+		for(int i = y; i > 0; i--) {
+			if(theBoard.getPieceAtPosition(new int[]{x,i}) != z) {
+				possibleMoves.add(String.valueOf(x) + String.valueOf(i));
+			}
+			else
+				break;
+		}
 	}
-
+	
 }
