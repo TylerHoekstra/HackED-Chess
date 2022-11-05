b12ef9f96d157505b5832af4c4715083c8d68506
Isaac Joffe <isaac11joffe@gmail.com>
2022-11-05 15:51:56 -0600

    Update King.java

--- HackED-Chess/src/King.java
+++ HackED-Chess/src/King.java
@@ -13,8 +13,57 @@ public class King extends Piece{
 
 	@Override
 	public void findPossibleMoves(Board theBoard) {
-		// TODO Auto-generated method stub
-		
+		int x = this.getPosition()[0];
+		int y = this.getPosition()[1];
+		possibleMoves.clear();
+		if (x <= 6 && y <= 6) {
+			int[] spot = {x + 1, y + 1};
+			if ((getIsBlack() && (theBoard.getPieceAtPosition(spot) <= 0)) || (!getIsBlack() && (theBoard.getPieceAtPosition(spot) >= 0))) {
+				possibleMoves.add(String.valueOf(spot[0]) + String.valueOf(spot[1]));
+			}
+		}
+		if (x <= 6) {
+			int[] spot = {x + 1, y};
+			if ((getIsBlack() && (theBoard.getPieceAtPosition(spot) <= 0)) || (!getIsBlack() && (theBoard.getPieceAtPosition(spot) >= 0))) {
+				possibleMoves.add(String.valueOf(spot[0]) + String.valueOf(spot[1]));
+			}
+		}
+		if (x <= 6 && y >= 1) {
+			int[] spot = {x + 1, y - 1};
+			if ((getIsBlack() && (theBoard.getPieceAtPosition(spot) <= 0)) || (!getIsBlack() && (theBoard.getPieceAtPosition(spot) >= 0))) {
+				possibleMoves.add(String.valueOf(spot[0]) + String.valueOf(spot[1]));
+			}
+		}
+		if (y >= 1) {
+			int[] spot = {x, y - 1};
+			if ((getIsBlack() && (theBoard.getPieceAtPosition(spot) <= 0)) || (!getIsBlack() && (theBoard.getPieceAtPosition(spot) >= 0))) {
+				possibleMoves.add(String.valueOf(spot[0]) + String.valueOf(spot[1]));
+			}
+		}
+		if (x >= 1 && y >= 1) {
+			int[] spot = {x - 1, y - 1};
+			if ((getIsBlack() && (theBoard.getPieceAtPosition(spot) <= 0)) || (!getIsBlack() && (theBoard.getPieceAtPosition(spot) >= 0))) {
+				possibleMoves.add(String.valueOf(spot[0]) + String.valueOf(spot[1]));
+			}
+		}
+		if (x >= 1) {
+			int[] spot = {x - 1, y};
+			if ((getIsBlack() && (theBoard.getPieceAtPosition(spot) <= 0)) || (!getIsBlack() && (theBoard.getPieceAtPosition(spot) >= 0))) {
+				possibleMoves.add(String.valueOf(spot[0]) + String.valueOf(spot[1]));
+			}
+		}
+		if (x >= 1 && y <= 6) {
+			int[] spot = {x - 1, y + 1};
+			if ((getIsBlack() && (theBoard.getPieceAtPosition(spot) <= 0)) || (!getIsBlack() && (theBoard.getPieceAtPosition(spot) >= 0))) {
+				possibleMoves.add(String.valueOf(spot[0]) + String.valueOf(spot[1]));
+			}
+		}
+		if (y <= 6) {
+			int[] spot = {x, y + 1};
+			if ((getIsBlack() && (theBoard.getPieceAtPosition(spot) <= 0)) || (!getIsBlack() && (theBoard.getPieceAtPosition(spot) >= 0))) {
+				possibleMoves.add(String.valueOf(spot[0]) + String.valueOf(spot[1]));
+			}
+		}
 	}
 
 }
