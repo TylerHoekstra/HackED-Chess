e1f9f38185ce956ba96af9848329adcced7e2709
Isaac Joffe <isaac11joffe@gmail.com>
2022-11-05 16:01:18 -0600

    Update Bishop.java

--- HackED-Chess/src/Bishop.java
+++ HackED-Chess/src/Bishop.java
@@ -14,7 +14,45 @@ public class Bishop extends Piece{
 	@Override
 	public void findPossibleMoves(Board theBoard) {
 		// TODO Auto-generated method stub
-		
+		int x = this.getPosition()[0];
+		int y = this.getPosition()[1];
+		possibleMoves.clear();
+		int colour;
+		if(isBlack)
+			colour = 1;
+		else
+			colour = -1;
+		// Right
+		for(int i = x; i < 8; i++) {
+			if(theBoard.getPieceAtPosition(new int[]{i,y+i-x}) != colour && y+i-x >= 0 && y+i-x <= 7) {
+				possibleMoves.add(String.valueOf(i) + String.valueOf(y+i-x));
+			}
+			else
+				break;
+		}
+		// Left
+		for(int i = x; i > 0; i--) {
+			if(theBoard.getPieceAtPosition(new int[]{i,y+i-x}) != colour && y+i-x >= 0 && y+i-x <= 7) {
+				possibleMoves.add(String.valueOf(i) + String.valueOf(y+i-x));
+			}
+			else
+				break;
+		}
+		// Up
+		for(int i = y; i < 8; i++) {
+			if(theBoard.getPieceAtPosition(new int[]{x+i-y,i}) != colour && x+i-y >= 0 && x+i-y <= 7) {
+				possibleMoves.add(String.valueOf(x+i-y) + String.valueOf(i));
+			}
+			else
+				break;
+		}
+		// Down
+		for(int i = y; i > 0; i--) {
+			if(theBoard.getPieceAtPosition(new int[]{x+i-y,i}) != colour && x+i-y >= 0 && x+i-y <= 7) {
+				possibleMoves.add(String.valueOf(x+i-y) + String.valueOf(i));
+			}
+			else
+				break;
+		}
 	}
-
 }
