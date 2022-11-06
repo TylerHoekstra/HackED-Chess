dfe6070973033358a92138ab8a43ce4b87ab402d
Isaac Joffe <isaac11joffe@gmail.com>
2022-11-05 19:34:16 -0600

    Update Game.java

--- HackED-Chess/src/Game.java
+++ HackED-Chess/src/Game.java
@@ -2,9 +2,25 @@ public class Game {
 	
 	public static void main(String[] args) {
 		Board newGame = new Board();
-		Player
-		Player
-
+		Player p1 = new Player(false);    // white
+		Player p2 = new Player(true);    // black
+		boolean isOver = false;
+		boolean turn = true;
+		int winner = 0;
+		while (!isOver) {
+			if (turn) {
+				p1.move();
+			} else {
+				p2.move();
+			}
+			if (newGame.inCheckmate()) {
+				winner = 1;
+				break;
+			} else if (newGame.inStalemate()) {
+				winner = 0;
+				break;
+			}
+		}
 	}
 
 }
