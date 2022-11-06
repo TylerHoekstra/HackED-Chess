7f389723312781e6db28bd6a56a49ea8c97d04e9
TylerHoekstra <89994695+TylerHoekstra@users.noreply.github.com>
2022-11-05 21:03:54 -0600

    pipes semi imp

--- HackED-Chess/src/Game.java
+++ HackED-Chess/src/Game.java
@@ -1,6 +1,50 @@
+import java.nio.channels.Pipe;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+
 public class Game {
 	
+	public static void inputPipe(){
+		try {
+			Pipe pipe = Pipe.open();
+			Pipe.SinkChannel sink = pipe.sink();
+			String test = "This is a test";
+			ByteBuffer buffer = ByteBuffer.allocate(1024);
+			buffer.clear();
+			buffer.put(test.getBytes());
+			buffer.flip();
+			while(buffer.hasRemaining()) {
+		         sink.write(buffer);
+		    }
+		}
+		catch(IOException e) {
+			System.err.println("Pipe IOException");
+		}
+	}
+	
+	public static void outputPipe(){
+		try {
+			Pipe pipe = Pipe.open();
+			Pipe.SourceChannel source = pipe.source();
+			ByteBuffer buffer = ByteBuffer.allocate(1024);
+			while(source.read(buffer) > 0){
+				 buffer.flip();
+		         while(buffer.hasRemaining()){
+		            char ch = (char) buffer.get();
+		            System.out.print(ch);
+		         }
+		         buffer.clear();
+		    }
+		}
+		catch(IOException e) {
+			System.err.println("Pipe IOException");
+		}
+	}
+	
 	public static void main(String[] args) {
+		
+		inputPipe();
+		outputPipe();
 		Board newGame = new Board();
 		Player p1 = new Player(false, newGame);    // white
 		Player p2 = new Player(true, newGame);    // black
